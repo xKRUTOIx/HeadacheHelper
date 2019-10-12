ef4a7de2ca788e9aeece5a1c0a74a707b2e67151
xkrutoix <xkrutoix@gmail.com>
2019-10-12 15:28:35 +0300

    add detailed history

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import mongo
 import constants
 import datetime
 import redis_key
+import ujson
+import pytz
 
 from config import BOT_TOKEN
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, CallbackQueryHandler
@@ -73,18 +75,50 @@ def callbacks(bot, update):
         results = mongo.get_statistic(user_id, callback_data)
         if results is not None:
             results = list(results)
+            r.set(redis_key.HISTORY_REQUEST + str(user_id), ujson.dumps(results))
             pretty_history_msg = pretty_history(results, callback_data)
-            bot.send_message(user_id, pretty_history_msg, reply_markup=keyboards.HISTORY_KEYBOARD, parse_mode=ParseMode.MARKDOWN)
+            bot.edit_message_text(chat_id=user_id, text=pretty_history_msg, message_id=msg_id, reply_markup=keyboards.HISTORY_KEYBOARD, parse_mode=ParseMode.MARKDOWN)
+    elif callback_data.startswith(constants.DETAILED_HISTORY):
+        if callback_data == constants.DETAILED_HISTORY_PAINFREE_CB:
+            filter_res = constants.NO_HURT_CB
+        else:
+            filter_res = constants.YES_HURT_CB
+
+        results = r.get(redis_key.HISTORY_REQUEST + str(user_id))
+        if results is None:
+            bot.send_message(user_id, "Ошибка при попытке получить информацию из базы данных.")
+        results = ujson.loads(results)
+        msg_text = pretty_detailed_history(results, filter_res)
+        bot.send_message(user_id, msg_text, parse_mode=ParseMode.MARKDOWN)
 
     return
 
 
+def pretty_detailed_history(results, filter_res):
+    reply_text = "*Детальная статистика.*\n"
+
+    for res in results:
+        history_data = res[mongo.HEADACHE_HISTORY]
+        did_hurt = history_data[mongo.DID_HURT]
+        res_date = datetime.datetime.fromtimestamp(history_data[mongo.TIME])
+        pretty_date = format_datetime(res_date, "dd MMMM YYYY", locale="ru_RU")
+
+        if filter_res == did_hurt:
+            pills = history_data[mongo.ATE_PILLS]
+            if pills == constants.YES_PILLS_CB:
+                pills_text = "да"
+            else:
+                pills_text = "нет"
+            reply_text += "[{}]: баллы: {}, таблетки: {}, комментарий: {}\n".\
+                format(pretty_date, history_data[mongo.HURT_RATE], pills_text, history_data[mongo.COMMENT])
+
+    return reply_text
+
+
 def get_timestamp(user_id):
     timestamp = r.get(redis_key.LAST_DAY_TIMESTAMP + str(user_id))
-    print(timestamp, type(timestamp))
     if timestamp is not None:
         timestamp = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
-        print(timestamp, type(timestamp))
         return timestamp
     else:
         return datetime.datetime.now()
@@ -111,7 +145,7 @@ def messages_handler(bot, update):
     if r.get(redis_key.WAITING_FOR_COMMENT + str(user_id)):
         message_text = update.message.text
         mongo.update_data(user_id, constants.YES_HURT_CB, hurt_rate=r.get(redis_key.HURT_RATE + str(user_id)),
-                          pills=r.get(redis_key.PILLS + str(user_id)), comment=message_text, timestamp=r.get(redis_key.LAST_DAY_TIMESTAMP + str(user_id)))
+                          pills=r.get(redis_key.PILLS + str(user_id)), comment=message_text, timestamp=get_timestamp(user_id))
         bot.send_message(chat_id=user_id, text=messages.THANKS_MESSAGE)
         r.delete(redis_key.WAITING_FOR_COMMENT + str(user_id))
         r.delete(redis_key.PILLS + str(user_id))
@@ -169,8 +203,8 @@ def pretty_history(history, period):
     painful_days = 0
     painfree_days = 0
     if period == constants.ALL_TIME_CB:
-        start_date = history[0]['headache_history']['time']
-        end_date = history[len(history) - 1]['headache_history']['time']
+        start_date = history[0][mongo.HEADACHE_HISTORY][mongo.TIME]
+        end_date = history[len(history) - 1][mongo.HEADACHE_HISTORY][mongo.TIME]
     elif period == constants.THIS_MONTH_CB:
         start_date = datetime.datetime.today().replace(day=1)
         end_date = datetime.datetime.today()
@@ -186,7 +220,7 @@ def pretty_history(history, period):
     missed_days = amount_of_days - len(history) + 1
 
     for res in history:
-        did_hurt = res['headache_history']['did_hurt']
+        did_hurt = res[mongo.HEADACHE_HISTORY][mongo.DID_HURT]
         if did_hurt == constants.YES_HURT_CB:
             painful_days += 1
         else:
